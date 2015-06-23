9ff6fb1cea19d0da479d0d3c18f3bd1618212a1c
leontian <l.tian.email@gmail.com>
2015-06-23 05:35:54 -0400

    lecture 2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,7 +14,38 @@ class ViewController: UIViewController {
     var usrIsInTheMiddleOfTypingADigit = false
     
     @IBAction func operate(sender: UIButton) {
+        if usrIsInTheMiddleOfTypingADigit {
+            enter()
+        }
+        switch(sender.currentTitle!) {
+        case "+":
+            performOperation { $0 + $1 }
+        case "÷":
+            performOperation { $1 / $0 }
+        case "✕":
+            performOperation(*)
+        case "−":
+            performOperation { $1 / $0 }
+        case "√":
+            performOperation(sqrt)
+        default:
+            break
+        }
     
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
     }
     
     var operandStack = [Double]()
@@ -22,6 +53,7 @@ class ViewController: UIViewController {
     @IBAction func enter() {
         usrIsInTheMiddleOfTypingADigit = false
         operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
     }
     
     @IBAction func appendDigit(sender: UIButton) {
